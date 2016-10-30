e97fc3fa63a5ac4719e44f627e9906b81e6ca407
shotat <shotat@users.noreply.github.com>
2016-10-30 12:37:52 +0900

    add lcs

--- sample/lcs.cpp
+++ sample/lcs.cpp
@@ -11,14 +11,15 @@
 
 using namespace std;
 
-#define MAX_N 100
-#define MAX_M 100
+#define MAX_N 1000
+#define MAX_M 1000
 
 /*
  * define global variables here.
  */
 int N, M;
-vector<vector<bool>> field;
+string s, t;
+vector<vector<int>> dp;
 
 void solve();
 void setup();
@@ -30,19 +31,41 @@ int main()
   return 0;
 }
 
+int lcs(int s_idx, int t_idx) {
+  // guard
+  if (dp[s_idx][t_idx] >= 0) { return dp[s_idx][t_idx]; }
+  if (s_idx >= N) { return 0; }
+  if (t_idx >= M) { return 0; }
+  
+  // solve
+  int res;
+  if (s[s_idx] == t[t_idx]) {
+     res = 1 + lcs(s_idx + 1, t_idx + 1);
+  } else {
+    res = max(
+        lcs(s_idx, t_idx + 1),
+        lcs(s_idx + 1, t_idx)
+        );
+  }
+  return dp[s_idx][t_idx] = res;
+}
 void setup() {
   // make io faster.
   cin.tie(0);
   ios::sync_with_stdio(false);
+  N = 4;
+  M = 4;
+  s = "abcd";
+  t = "becd";
 
   /*
    * initialize global variables here.
    */
 
+  dp = vector<vector<int>>(N + 1, vector<int>(M + 1, -1));
   /*
    * sample
    cin >> N >> M;
-   field = vector<vector<bool>>(N, vector<bool>(M, false));
    REP(i, N) {
    string s;
    cin >> s;
@@ -57,6 +80,7 @@ void setup() {
 }
 
 void solve() {
+  cout << lcs(0,0) << endl;
   /*
    * answer is here.
    */
